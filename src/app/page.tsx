b391276c6b3531f483c7f7d5c33e396c9c779a05
Prakash7895 <prakashchoudhary0141@gmail.com>
2023-10-29 11:28:39 +0530

    comments removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-import AdSpace from '@/components/AdSpace';
 import OnlinePlayers from '@/components/OnlinePlayers';
 import StartCTAs from '@/components/StartCTAs';
 import Welcome from '@/components/Welcome';
@@ -8,12 +7,9 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-10'>
       <h1 className='text-5xl'>Three Men's Morris</h1>
-      {/* <div className='flex h-full flex-1 justify-between w-full items-center '> */}
-      {/* <AdSpace /> */}
       <Welcome />
       <StartCTAs />
       <OnlinePlayers />
-      {/* </div> */}
     </main>
   );
 }
