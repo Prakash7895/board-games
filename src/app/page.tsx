5f527ed54a1357232227cac3325c88766b0a57f8
Prakash7895 <prakashchoudhary0141@gmail.com>
2023-11-07 11:45:58 +0530

    eslint fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
 
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-10'>
-      <h1 className='text-5xl'>Three Men's Morris</h1>
+      <h1 className='text-5xl'>Three Men&apos;s Morris</h1>
       <Welcome />
       <StartCTAs />
       <OnlinePlayers />
